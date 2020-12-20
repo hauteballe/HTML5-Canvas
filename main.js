53d488c24a9c04b8e9ba5a41ca7eb7b1f3d46289
Irina Sinitsyna <hauteballe@gmail.com>
2020-12-20 19:00:20 +0300

    feat: implement color picker and width changing

--- main.js
+++ main.js
@@ -1,42 +1,35 @@
 const canvas = document.querySelector("#draw");
-const ctx = canvas.getContext("2d");
+const context = canvas.getContext("2d");
+const inputs = document.querySelectorAll(".menu input");
+let widthPicker = document.querySelector("#width-size");
+let colorPicker = document.querySelector("#color-panel");
+
 canvas.width = window.innerWidth;
 canvas.clientHeight = window.innerHeight;
-ctx.strokeStyle = "#BADA55";
-ctx.lineJoin = "round";
-ctx.lineCap = "round";
-ctx.lineWidth = 60;
+
+context.strokeStyle = "#006EFF";
+context.lineJoin = "round";
+context.lineCap = "round";
+context.lineWidth = 60;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-let hue = 0;
 let direction = true;
 
 function draw(elem) {
   if (!isDrawing) return;
-
   console.log(elem);
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  ctx.lineWidth = hue;
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(elem.offsetX, elem.offsetY);
-  ctx.stroke();
-  [lastX, lastY] = [elem.offsetX, elem.offsetY];
 
-  hue++;
-  if (hue >= 360) {
-    hue = 0;
-  }
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    direction = !direction;
-  }
-  if (direction) {
-    ctx.lineWidth++;
-  } else {
-    ctx.lineWidth--;
-  }
+  context.strokeStyle = colorPicker.value;
+  context.lineWidth = widthPicker.value;
+
+  context.beginPath();
+  context.moveTo(lastX, lastY);
+  context.lineTo(elem.offsetX, elem.offsetY);
+  context.stroke();
+
+  [lastX, lastY] = [elem.offsetX, elem.offsetY];
 }
 
 canvas.addEventListener("mousedown", (elem) => {
@@ -49,6 +42,6 @@ canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
 
 document.getElementById("clear").onclick = function () {
-  ctx.clearRect(0, 0, 950, 450);
-  ctx.beginPath();
+  context.clearRect(0, 0, 950, 450);
+  context.beginPath();
 };
